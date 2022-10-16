8162b201d69915291670144011de86dc58f2bb60
Александр Беликов <abelikov@samokat.ru>
2022-10-16 11:37:06 +0600

    fixs for deploy

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("dist"));
+
+app.set("trust proxy", 1);
+
+app.get("/healthcheck", (req, res) => {
+  res.end("ok");
+});
+
+app.get("*", function(req, res){
+  res.sendfile("dist/index.html");
+});
+
+app.listen(8080, () => {
+  console.log("listening on port 8080");
+});
